2c4e3f00b5cfc53c249af04a0ed2eeb591ef7442
adrianmoser <adrianmoser8@gmail.com>
2019-09-13 10:42:19 +0200

    struktur fertig

---
+++ src/main/java/at/htl/football/League.java
@@ -0,0 +1,20 @@
+package at.htl.football;
+
+import java.util.List;
+
+public class League {
+
+    private List<Team>;
+
+    public void addMatchResult(Match) {
+
+    }
+
+    private Team findOrCreateTeam(String) {
+
+    }
+
+    public List<Team> getTable() {
+
+    }
+}
---
+++ src/main/java/at/htl/football/Main.java
@@ -0,0 +1,15 @@
+package at.htl.football;
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    private void printTable(List<Team>) {
+
+    }
+
+}
